fde5adbbd08f73249c91b7fd8cdbe7a0ae127b54
Matan Lurey <matan@lurey.org>
2020-10-26 22:30:30 -0700

    Add data sample.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { Layout } from 'antd';
+import data from '@retinue/databank/data.json';
+import { Layout, Menu } from 'antd';
 import React from 'react';
 import './App.scss';
 
 export default function App() {
-  return <Layout style={{ minHeight: '100vh', height: '100%' }}>Test!</Layout>;
+  console.log(data);
+  return (
+    <Layout style={{ minHeight: '100vh', height: '100%' }}>
+      <Layout.Sider collapsible>
+        <Menu theme="dark" />
+      </Layout.Sider>
+      <Layout className="site-layout">
+        <Layout.Content style={{ padding: '10px' }}>Test</Layout.Content>
+      </Layout>
+    </Layout>
+  );
 }
