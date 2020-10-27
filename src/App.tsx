ffc77ce065eaace77f393d597a248b4bcfea3475
Matan Lurey <matan@lurey.org>
2020-10-26 20:41:43 -0700

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Layout } from 'antd';
+import React from 'react';
+import './App.scss';
+
+export default function App() {
+  return <Layout style={{ minHeight: '100vh', height: '100%' }}>Test!</Layout>;
+}
