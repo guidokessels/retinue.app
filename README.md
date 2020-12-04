5f978e6381365d0d0db17748e810131a69e7d9fa
Matan Lurey <matan@lurey.org>
2020-12-03 22:31:51 -0800

    Fix and use latest versions.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Retinue.app
+
+## TODO
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import data from '@retinue/databank/data.json';
 import { Layout, Menu } from 'antd';
 import React from 'react';
 import './App.scss';
 
 export default function App() {
-  console.log(data);
   return (
     <Layout style={{ minHeight: '100vh', height: '100%' }}>
       <Layout.Sider collapsible>
