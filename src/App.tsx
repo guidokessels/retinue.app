e81fa561ccb9a21f4f853b0b7e7d55df3e0f0b24
Matan Lurey <matan@lurey.org>
2020-12-05 19:44:01 -0800

    Tweak and remove .eslintcache.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,19 @@ export default function App() {
   return (
     <Layout style={{ minHeight: '100vh', height: '100%' }}>
       <Layout.Sider collapsible>
+        <div
+          style={{
+            height: '32px',
+            margin: '16px',
+            background: 'rgba(255, 255, 255, 0.3)',
+            textAlign: 'center',
+            lineHeight: '32px',
+            fontWeight: 'bold',
+            color: '#fff',
+          }}
+        >
+          Retinue
+        </div>
         <Menu theme="dark" />
       </Layout.Sider>
       <Layout className="site-layout">
